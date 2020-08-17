dc32ddb3eb217bddd9c1a795137e5a0160eae251
chocolate <phamduychien25042001@gmail.com>
2020-08-17 09:22:57 +0700

    update-2

---
+++ root/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+			$("#btn-close").click(function() {
+				$(".full").hide();
+				$(".popup").hide();
+			});
+			$(".full").click(function() {
+				$(".full").hide();
+				$(".popup").hide();
+			});
+			$("#men").click(function() {
+				$(".men").toggle();
+			});
+			$("#women").click(function() {
+				$(".women").toggle();
+			});
+			$("#blog").click(function() {
+				$(".box-blog").toggle();
+			});
+			$("#page").click(function() {
+				$(".page").toggle();
+			});
+			$("#cart").click(function() {
+				$(".cart-drop").toggle();
+			});
+			$("#search").click(function() {
+				$(".box-search").toggle();
+			});
+			$("#usa").click(function() {
+				$(".drop-usa").toggle();
+			});
+			$("#eng").click(function() {
+				$(".drop-eng").toggle();
+			});
+		});
